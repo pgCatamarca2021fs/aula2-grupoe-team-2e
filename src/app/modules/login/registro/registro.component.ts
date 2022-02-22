84965a00072c3a90a951421d1ef0e1049a70f8fc
Lionel Diez Gómez <satur.1512@gmail.com>
2022-02-21 23:54:46 -0300

    ajustes parciales del formulario

--- src/app/modules/login/registro/registro.component.ts
+++ src/app/modules/login/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Usuario from '../interfaces/usuario.interface';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  public usuario: Usuario = {
+    name: '',
+    lastName: '',
+    password: '',
+    passwordRepeat: '',
+    birthday: '1995-12-14',
+    email: '',
+  }
 
   constructor() { }
 
+  agregarUsuario(){
+    console.log("hola",this.usuario.birthday)
+  }
+
   ngOnInit() {
+    let dateToday = new Date()
+    this.usuario.birthday = String(dateToday.getFullYear() + '-' + String(dateToday.getMonth() + 1).padStart(2, '0') + '-' + dateToday.getDate()).padStart(2, '0');
   }
 
 }
