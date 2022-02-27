aaf296fa14e7509d0e1abd33cf7d661d45a5deb8
Lionel Diez Gómez <satur.1512@gmail.com>
2022-02-27 17:16:38 -0300

    avances con el formulario

--- src/app/modules/login/registro/registro.component.ts
+++ src/app/modules/login/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import Usuario from '../interfaces/usuario.interface';
 
 @Component({
@@ -8,46 +8,68 @@ import Usuario from '../interfaces/usuario.interface';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
+  public dateToday!: string
   formRegistracion: FormGroup = new FormGroup({});
 
-  public usuario: Usuario = {
-    name: '',
-    lastName: '',
-    password: '',
-    passwordRepeat: '',
-    birthday: '1995-12-14',
-    email: '',
-  }
-
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   agregarUsuario(){
-    console.log("hola",this.usuario.birthday);
+    console.log("hola",this.formRegistracion.value);  
   }
 
   ngOnInit() {
-    let dateToday = new Date()
-    this.usuario.birthday = String(dateToday.getFullYear() + '-' + String(dateToday.getMonth() + 1).padStart(2, '0') + '-' + dateToday.getDate()).padStart(2, '0');
+    const date: Date = new Date
+    this.dateToday = String(date.getFullYear() + '-' + 
+      String(date.getMonth() + 1).padStart(2, '0') + '-' + 
+      date.getDate()).padStart(2, '0');
 
-    this.formRegistracion = new FormGroup({
-      name: new FormControl('',[
+    this.formRegistracion = this.formBuilder.group({
+      name: this.formBuilder.control('',[
         Validators.required,
+        Validators.minLength(2),
+        Validators.pattern("^[^0-9]*$") //no numeros
       ]),
       lastName: new FormControl('',[
         Validators.required,
+        Validators.minLength(2),
+        Validators.pattern("^[^0-9]*$")
       ]),
       password: new FormControl('',[
         Validators.required,
         Validators.minLength(4),
         Validators.maxLength(12),
-        Validators.pattern("^[0-9]*$")
       ]),
       passwordRepeat: new FormControl('',[
         Validators.required,
-        // Validators.
+        this.controlValuesAreEqual('password','passwordRepeat')
       ]),
+      birthday: new FormControl(this.dateToday,[
+        Validators.required
+      ]),
+      email: new FormControl('',[
+        Validators.required,
+        Validators.email,
+      ]),
+      dni: new FormControl('',[
+        Validators.required,
+        Validators.min(9999999),
+      ])
     });
   }
+
+  private controlValuesAreEqual(controlNameA: string, controlNameB: string): ValidatorFn {
+    return (): ValidationErrors | null => {
+      // const formGroup = control as FormGroup;
+      const valueOfControlA = this.formRegistracion.get(controlNameA)?.value;
+      const valueOfControlB = this.formRegistracion.get(controlNameB)?.value;
+      // console.log('a: ',valueOfControlA,'b: ',valueOfControlB)
+
+      if (valueOfControlA === valueOfControlB) {
+        return null
+      } else {
+        return { valuesDoNotMatch: true }
+      };
+    };
+  };
 
 }
