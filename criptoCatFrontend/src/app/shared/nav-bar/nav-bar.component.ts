17920fb420277fc0822f57dc55159107302f77cd
Pablo <pablosarisnicolas@gmail.com>
2022-03-24 14:38:28 -0300

    asdasdasdasdasd ng if asdasdas

--- criptoCatFrontend/src/app/shared/nav-bar/nav-bar.component.ts
+++ criptoCatFrontend/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faCat } from '@fortawesome/free-solid-svg-icons';
 
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -9,10 +10,12 @@ import { faCat } from '@fortawesome/free-solid-svg-icons';
 export class NavBarComponent implements OnInit {
 
   constructor() { }
-
+  Nombre:string ="";
   faCat = faCat;
-
+  
   ngOnInit(): void {
+    this.Nombre = '';
+    
   }
 
 }
