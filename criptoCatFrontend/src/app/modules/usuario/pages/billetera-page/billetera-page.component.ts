1918594792815c06afa8c38c3b700417f651f9f8
Pablo <pablosarisnicolas@gmail.com>
2022-03-25 22:49:40 -0300

    bubu y nick intentado de llenar llenar de bug no de arreglarlos

--- criptoCatFrontend/src/app/modules/usuario/pages/billetera-page/billetera-page.component.ts
+++ criptoCatFrontend/src/app/modules/usuario/pages/billetera-page/billetera-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Billetera } from 'src/app/shared/interfaces/usuarioCache.interface';
 import { CacheService } from 'src/app/shared/services/cache.service';
 @Component({
   selector: 'app-billetera-page',
@@ -9,8 +10,12 @@ export class BilleteraPageComponent implements OnInit {
 
   constructor(private cacheService:CacheService) { }
   pesosaldoarg:number =0;
+  billeteras!: Array<Billetera>;
+
   ngOnInit(): void {
     this.pesosaldoarg= this.cacheService.usuarioCache.cuenta[0].billeteras[0].monto_dinero;
+    this.billeteras = this.cacheService.usuarioCache.cuenta[0].billeteras;
+    this.billeteras.shift()
   }
 
 }
