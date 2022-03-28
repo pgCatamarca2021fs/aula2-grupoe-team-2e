c8d5cb4f2eeb2e833837c158304da02be57f95f5
Lionel Diez Gómez <satur.1512@gmail.com>
2022-03-27 23:27:56 -0300

    sistema de compra completo BE

--- criptoCatBackend/criptoCatBackend/Models/GestorOperaciones.cs
+++ criptoCatBackend/criptoCatBackend/Models/GestorOperaciones.cs
@@ -12,6 +12,8 @@ namespace criptoCatBackend.Models
     {
         string conectionString = ConfigurationManager.ConnectionStrings["BDCriptoCat"].ToString(); //cadena de conexión
 
+        BDCriptoCatEntities db = new BDCriptoCatEntities();
+
         public void CrearOperacion(int id , FormOperacion datosEntrada )
         {
             using (SqlConnection connection = new SqlConnection(this.conectionString))
@@ -34,6 +36,37 @@ namespace criptoCatBackend.Models
                 command2.Parameters.Add(new SqlParameter("@idUsuario", id));
                 command2.ExecuteNonQuery();
 
+                //pregunto si existe la billetera
+                cuenta oCuenta = db.cuenta.Where(u => u.id_usuario == id).FirstOrDefault();
+                billeteras oBilleteraCripto = db.billeteras.Where(u => u.id_cuenta == oCuenta.id_cuenta &&
+                u.moneda.tipo == datosEntrada.coinSelected.id.ToUpper()).FirstOrDefault();
+                if (oBilleteraCripto == null)
+                {
+                    SqlCommand commandBilletera = connection.CreateCommand();
+                    commandBilletera.CommandText = "crearBilletera";
+                    commandBilletera.CommandType = CommandType.StoredProcedure;
+                    commandBilletera.Parameters.Add(new SqlParameter("@idCuenta", oCuenta.id_cuenta));
+                    commandBilletera.Parameters.Add(new SqlParameter("@idMoneda", oBilleteraCripto.moneda.id_moneda));
+                    commandBilletera.Parameters.Add(new SqlParameter("@cantidadCripto", datosEntrada.cantidadCripto));
+                    command2.ExecuteNonQuery();         
+                }
+                else
+                {
+                    oBilleteraCripto.monto_dinero = (decimal?)((float)oBilleteraCripto.monto_dinero + datosEntrada.cantidadCripto);
+                    db.billeteras.Attach(db.billeteras.Single(b => b.id_cuenta == oCuenta.id_cuenta &&
+                    b.moneda.tipo == datosEntrada.coinSelected.id.ToUpper()));
+                    db.Entry(oBilleteraCripto).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                //restar plata de la billetera
+                billeteras billeteraPesos = db.billeteras.Where(b => b.id_cuenta == oCuenta.id_cuenta
+                    && b.id_moneda == 1).FirstOrDefault();
+                billeteraPesos.monto_dinero = (decimal?)((float)billeteraPesos.monto_dinero - datosEntrada.cantidadPesos);
+
+                db.billeteras.Attach(db.billeteras.Single(b => b.id_cuenta == oCuenta.id_cuenta && b.id_moneda == 1));
+                db.Entry(billeteraPesos).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
             }
         }
 
