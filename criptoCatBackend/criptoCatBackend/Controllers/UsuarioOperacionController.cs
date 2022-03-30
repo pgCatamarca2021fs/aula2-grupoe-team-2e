63ecc40165e83e13da67de63158e5b877e6aaa9e
Lionel Diez Gómez <satur.1512@gmail.com>
2022-03-29 23:31:09 -0300

    compra unida con el front

--- criptoCatBackend/criptoCatBackend/Controllers/UsuarioOperacionController.cs
+++ criptoCatBackend/criptoCatBackend/Controllers/UsuarioOperacionController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using criptoCatBackend.Models;
 
 namespace criptoCatBackend.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsuarioOperacionController : ApiController
     {
         // GET api/<controller>
